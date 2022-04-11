f9b1da801f7e7a55e98c1ad553be729c8078fc5d
jasonjohnson47 <jasonjohnson47@gmail.com>
2022-04-10 22:28:59 -0400

    Uninstall and rip out ng-zorro ant design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameSetupComponent } from './game-setup/game-setup.component';
-import { NzFormModule } from 'ng-zorro-antd/form';
 import { NativeElementInjectorDirective } from './native-element-injector.directive';
 import { CurrentRoundComponent } from './current-round/current-round.component';
 import { GameHistoryComponent } from './game-history/game-history.component';
@@ -36,9 +32,8 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzFormModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
